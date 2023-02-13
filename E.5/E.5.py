5e0b452ec5e64bcc686e7e58feae48b5b9c696e7
jose antonio <jsancesc@gmail.com>
2023-02-13 15:43:58 +0100

    commit2

--- E.5/E.5.py
+++ E.5/E.5.py
@@ -3,3 +3,17 @@
  A partir de 5 niños, el descuento es del 18 %, pero aumenta un 1 % por cada niño por encima de 4.
 Establecer el algoritmo que calcula el importe del descuento al que tendrá derecho una familia dada.
 """
+def solicitar_datos():
+    numero_de_ninos = int(input("Introduce el número de niños: "))
+    precio_de_la_entrada = float(input("Introduce el precio de la entrada: "))
+    return numero_de_ninos, precio_de_la_entrada
+def calcular_descuento(numero_de_ninos, precio_de_la_entrada):
+    if numero_de_ninos == 2:
+        descuento = 0.1
+    elif numero_de_ninos == 3:
+        descuento = 0.15
+    elif numero_de_ninos == 4:
+        descuento = 0.18
+    else:
+        descuento = 0.18 + (numero_de_ninos - 4) * 0.01
+    return ("El descuento es del", descuento * 100, "% y el precio final es de", precio_de_la_entrada * (1 - descuento), "€")
