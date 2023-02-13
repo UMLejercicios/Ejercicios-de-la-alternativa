efaa777ce85a2ecacebe71bbdb1e9691f56b886c
Nacho <isanccal@myuax.com>
2023-02-13 10:52:11 +0100

    Cambio 1

---
+++ E.4.py
@@ -0,0 +1,9 @@
+def evaluacion_alumno(nota1, nota2, nota3, nota4):
+    media = (nota1 + nota2 + nota3 + nota4) / 4
+    if media >= 15:
+        evaluacion = "Alumno con talento"
+    elif media >= 12:
+        evaluacion = "Alumno con capacidad"
+    else:
+        evalaucion = "Debe reorientarse"
+        
